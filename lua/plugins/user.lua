70107806188897513f2a72330935bf5dbe40a4fb
Wang Yunlong <wangyunlong.wyl@bytedance.com>
2024-09-09 01:30:44 +0800

    add aotu-session.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -77,4 +77,19 @@ return {
       )
     end,
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    dependencies = {
+      "nvim-telescope/telescope.nvim", -- Only needed if you want to use session lens
+    },
+
+    --enables autocomplete for opts
+    --@module "auto-session"
+    --@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
 }
